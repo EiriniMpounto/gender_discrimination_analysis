70dd9aff917a4648304e9c175c13c3d77bdda986
Irene Bounto <irene@scircula.com>
2022-06-14 22:48:26 +0200

    Creating a class that will contain all the functions plus adding new functions that calculate different kind of descriptives

--- descriptive_statistics.py
+++ descriptive_statistics.py
@@ -1,117 +1,208 @@
 import pandas as pd
 import seaborn as sns
 
-def gender_counts(data):
-    '''
-    The current function calculates the number
-    of males VS the number of the females that 
-    exist in our dataset.
-    '''
-    sex_counts = data['GENDER'].value_counts()
-    print('How many women and men we have on our dataset',sex_counts)
+
+class DescriptiveStatistics:
     
-    return sex_counts
-
-
-
-def ceo_counts(data):
-    '''
-    This function calsulates the number of the CEOs
-    and the number of the CFOs in the sample
-    '''
-    ceo_counts = data['PCEO'].value_counts()
-    cfo_counts = data['PCFO'].value_counts()
-    print('The number of the current CEOS in the dataset',ceo_counts)
-    print('The number of the current CFOs in the dataset', cfo_counts)
+    def gender_counts(data):
+        '''
+        The current function calculates the number
+        of males VS the number of the females that 
+        exist in our dataset.
+        '''
+        sex_counts = data['GENDER'].value_counts()
+        print('How many women and men we have on our dataset',sex_counts)
     
-    return ceo_counts, cfo_counts
+        return sex_counts
 
 
 
-def ceo_cfo_gender(data):
-    '''
-    The current function calculates the number
-    of women that possess the title of the CEO
-    VS the number of men that possess the same 
-    title at the current moment.(PCEO and PCFO)
-    '''
-    # ceo and gender
-    ceo_gender = data.groupby(['PCEO', 'GENDER'])['EXEC_FULLNAME']
-    calculate_ceo_gender = ceo_gender.count()
-    print('How many women and men have currently the title CEO',calculate_ceo_gender)
+    def ceo_counts(data):
+        '''
+        This function calsulates the number of the CEOs
+        and the number of the CFOs in the sample
+        '''
+        ceo_counts = data['PCEO'].value_counts()
+        cfo_counts = data['PCFO'].value_counts()
+        print('The number of the current CEOS in the dataset',ceo_counts)
+        print('The number of the current CFOs in the dataset', cfo_counts)
     
-    # cfo and gender
-    cfo_gender = data.groupby(['PCFO', 'GENDER'])['EXEC_FULLNAME']
-    calculate_cfo_gender = cfo_gender.count()
-    print('How many women and men have currently the CFO title', calculate_cfo_gender)
+        return ceo_counts, cfo_counts
+
+
+
+    def pceo_pcfo_gender(data):
+        '''
+        The current function calculates the number
+        of women that possess the title of the CEO
+        VS the number of men that possess the same 
+        title at the current moment.(PCEO and PCFO)
+        '''
+        # ceo and gender
+        ceo_gender = data.groupby(['PCEO', 'GENDER'])['EXEC_FULLNAME']
+        calculate_ceo_gender = ceo_gender.count()
+        print('How many women and men have currently the title CEO',calculate_ceo_gender)
+    
+        # cfo and gender
+        cfo_gender = data.groupby(['PCFO', 'GENDER'])['EXEC_FULLNAME']
+        calculate_cfo_gender = cfo_gender.count()
+        print('How many women and men have currently the CFO title', calculate_cfo_gender)
+        
+        return calculate_ceo_gender, calculate_cfo_gender
     
 
-def average_age_bygender(data):
-    '''
-    This function calculates the average age of women
-    in the entire database VS the average age of men.
-    '''
-    av_age = data.groupby('GENDER', as_index=False)['AGE'].mean()
-    print('The average age of women VS men is',av_age)
-    return av_age
+    def average_age_bygender(data):
+        '''
+        This function calculates the average age of women
+        in the entire database VS the average age of men.
+        '''
+        av_age = data.groupby('GENDER', as_index=False)['AGE'].mean()
+        print('The average age of women VS men is',av_age)
+        
+        return av_age
 
-def average_salary_by_gender(data):
-    '''
-    This function calculates the average salary of 
-    women VS men
-    '''
-    av_salary = data.groupby('GENDER', as_index = False)['SALARY'].mean()
-    print('The average salary of women VS men', av_salary)
+
+    def average_salary_by_gender(data):
+        '''
+        This function calculates the average salary of 
+        women VS men
+        '''
+        av_salary = data.groupby('GENDER', as_index = False)['SALARY'].mean()
+        print('The average salary of women VS men', av_salary)
     
-    return av_salary
+        return av_salary
 
-def average_sal_pct_gender(data):
-     '''
-     This function calculates the salry 
-     percentange change for women VS Men
-     '''
-     av_pct_sal = data.groupby('GENDER', as_index = False)['SAL_PCT'].mean()
-     print(av_pct_sal)
+
+    def average_sal_pct_gender(data):
+        '''
+        This function calculates the salry 
+        percentange change for women VS Men
+        '''
+        av_pct_sal = data.groupby('GENDER', as_index = False)['SAL_PCT'].mean()
+        print('The salary percentage change of women VS men',av_pct_sal)
      
-     return av_pct_sal
+        return av_pct_sal
 
-def av_total_compensation_gender(data):
-    '''
-    This function calculates the average of total 
-    compensation for men VS women
-    '''
-    av_tdc1_gender = data.groupby('GENDER', as_index= False)['TDC1'].mean()
-    av_tdc2_gender = data.groupby('GENDER', as_index = False)['TDC2'].mean()
-    
-    print('The total 1 year compensation by gender is:', av_tdc1_gender)
-    print('The total 2 year compensation by gender is:', av_tdc2_gender)
-    
-    return av_tdc1_gender, av_tdc2_gender
 
-def reason_leaving_gender(data):
-    '''
-    This function calculates the most common reason 
-    that women and men are leaving their company
-    '''
-    reason_gender = data.groupby(['REASON', 'GENDER'])['EXEC_FULLNAME']
-    reason_gender = reason_gender.count()
-    print(reason_gender)
-    return reason_gender
-
-def mode_spindex_spcode(data):
-    '''
-    This function calculates the mode of the 
-    spindex and the spcode variable
-    '''
-    spindex_mode = data['SPINDEX'].mode()
-    print('The mode for the SPINDEX variable is:', spindex_mode)
+    def av_total_compensation_gender(data):
+        '''
+        This function calculates the average of total 
+        compensation for men VS women
+        '''
+        av_tdc1_gender = data.groupby('GENDER', as_index= False)['TDC1'].mean()
+        av_tdc2_gender = data.groupby('GENDER', as_index = False)['TDC2'].mean()
     
-    spcode_mode = data['SPCODE'].mode()
-    print('The mode of the Scode variable is:', spcode_mode)
+        print('The total 1 year compensation by gender is:', av_tdc1_gender)
+        print('The total 2 year compensation by gender is:', av_tdc2_gender)
     
-    return spindex_mode, spcode_mode
+        return av_tdc1_gender, av_tdc2_gender
 
-def average_age_gender(data):
-    '''
-    This function calculates the average age between
-    men and women '''
+
+    def reason_leaving_gender(data):
+        '''
+        This function calculates the most common reason 
+        that women and men are leaving their company
+        '''
+        reason_gender = data.groupby(['REASON', 'GENDER'])['EXEC_FULLNAME']
+        reason_gender = reason_gender.count()
+        print('The most common reason that men and women are leaving the company', reason_gender)
+        
+        return reason_gender
+
+
+    def mode_spindex_spcode(data):
+        '''
+        This function calculates the mode of the 
+        spindex and the spcode variable
+        '''
+        spindex_mode = data['SPINDEX'].mode()
+        print('The mode for the SPINDEX variable is:', spindex_mode)
+    
+        spcode_mode = data['SPCODE'].mode()
+        print('The mode of the SPCODE variable is:', spcode_mode)
+    
+        return spindex_mode, spcode_mode
+
+
+    def average_age_gender_ceo(data):
+        '''
+        This function calculates the average age between
+        men and women that have currently the title CEO 
+        and the title CFO
+        '''
+        # average age women VS men CEOs
+        av_age_genderceo = data.groupby(['GENDER', 'PCEO'], as_index= False)['AGE'].mean()
+        print('The average age of men and women that have currently the title CEO',av_age_genderceo)
+        # average age women VS men CFOs
+        av_age_gendercfo = data.groupby(['GENDER', 'PCFO'], as_index = False)['AGE'].mean()
+        print('The average age of women and men that have currently the title CFO:', av_age_gendercfo)
+    
+        return 
+
+
+    def average_salary_of_ceo_by_gender(data):
+        '''
+        This function calculates the average salary between 
+        men and women that currently have the title CEO and CFO
+        '''
+        # average salary women VS men CEOs
+        av_salary_ceo_gender = data.groupby(['GENDER', 'PCEO'], as_index= False)['SALARY'].mean()
+        print('The average salary of women and men that have currently the title CEO',av_salary_ceo_gender)
+        # average salary women VS men CFOs
+        av_salary_cfo_gender = data.groupby(['GENDER','PCFO'], as_index= False)['SALARY'].mean()
+        print('The average salary of women and men that have currently the title CFO',av_salary_cfo_gender)
+        
+        return 
+    
+    
+    def ceo_cfo_gender(data):
+        '''
+        This function calculates the number of women 
+        and men that have the title of the CEO or the 
+        CFO at least once during their working career, 
+        and every other information that has been calculated 
+        previously for the current CEOs and CFOs
+        '''
+        
+        # number of ceos in the dataset
+        ceo_counts = data['CEO_NEW'].value_counts()
+        print('The number of executives that had at least once the title of the CEO:', ceo_counts)
+        
+        # number of the cfos in the dataset
+        cfo_counts = data['CFO_NEW'].value_counts()
+        print('The number of the executives that had at least once the title of the CFO:', cfo_counts)
+        
+        # ceo and gender
+        ceo_gender = data.groupby(['CEO_NEW', 'GENDER'])['EXEC_FULLNAME']
+        calculate_ceo_gender = ceo_gender.count()
+        print('How many women and men have had the title CEO at least once',
+              calculate_ceo_gender)
+    
+        # cfo and gender
+        cfo_gender = data.groupby(['CFO_NEW', 'GENDER'])['EXEC_FULLNAME']
+        calculate_cfo_gender = cfo_gender.count()
+        print('How many women and men have had the title CFO at least once', 
+              calculate_cfo_gender)
+        
+        # average age of ceos
+        av_age_ceos_gender = data.groupby(['GENDER', 'CEO_NEW'], as_index= False)['AGE'].mean()
+        print('The average age of women VS men that have had at least once the title CEO',
+              av_age_ceos_gender)
+        
+        # average age of cfos
+        av_age_cfos_gender = data.groupby(['GENDER', 'CFO_NEW'], as_index = False)['AGE'].mean()
+        print('The average age of women VS men that have had at least once the title CFO', 
+              av_age_cfos_gender)
+        
+        #average salary of ceos
+        av_sal_ceos_gender = data.groupby(['GENDER', 'CEO_NEW'], as_index= False)['SALARY'].mean()
+        print('The average salary of women Vs men that have had at least once the title CEO', 
+              av_sal_ceos_gender)
+        # average salary of cfos
+        av_sal_cfos_gender = data.groupby(['GENDER', 'CFO_NEW'], as_index= False)['SALARY'].mean()
+        print('The average salary of women Vs men that have had at least once the title CFO', 
+              av_sal_cfos_gender)
+        
+        
+        return 
+        
